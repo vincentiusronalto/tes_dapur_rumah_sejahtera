5231e9c3b6b10ee17fa1cf99aaf86c2df299d138
“Vincentius <vincentius.ronalto.d@gmail.com>
2021-06-30 14:02:45 +0700

    add no 2

--- coding_test2.js
+++ coding_test2.js
@@ -7,3 +7,26 @@ Input “abcde edcbza” output “bcde edcb”
 */
 
 //Jawaban no 2
+function longestPalindrome (str) {
+    const string = str.toLowerCase();
+    if (str.length < 2) return null;
+    let palindrome = '';
+  
+    function stringReverser (start, length) {
+      const reversed = string.substr(start, length).split('').reverse().join('');
+      return reversed;
+    }
+  
+    for (let i = 0; i < string.length; i++) {
+      for (let j = 1; j <= string.length - i; j++) {
+        if (string.substr(i, j) === stringReverser(i, j)) {
+          if (j > palindrome.length) {
+            palindrome = string.substr(i,j);
+          }
+        }
+      }
+    }
+  
+    if (!palindrome) return null;
+    return palindrome;
+  }
