7996b12b882fca931f90af9f82901c462c53da64
“Vincentius <vincentius.ronalto.d@gmail.com>
2021-06-30 14:18:06 +0700

    add

--- coding_test2.js
+++ coding_test2.js
@@ -7,26 +7,67 @@ Input “abcde edcbza” output “bcde edcb”
 */
 
 //Jawaban no 2
-function longestPalindrome (str) {
-    const string = str.toLowerCase();
-    if (str.length < 2) return null;
-    let palindrome = '';
-  
-    function stringReverser (start, length) {
-      const reversed = string.substr(start, length).split('').reverse().join('');
-      return reversed;
-    }
-  
-    for (let i = 0; i < string.length; i++) {
-      for (let j = 1; j <= string.length - i; j++) {
-        if (string.substr(i, j) === stringReverser(i, j)) {
-          if (j > palindrome.length) {
-            palindrome = string.substr(i,j);
-          }
+function expandAroundCenterSolution(s) {
+    let start = 0, end = 0;
+    for (let i = 0; i < s.length; i++) {
+        let center = getCenter(s, i);
+        let bounds = expandAroundCenter(s, center[0], center[1]);
+        let L = bounds[0], R = bounds[1];
+        if (R - L > end - start) {
+            start = L;
+            end = R;
         }
-      }
+
+        i = center[1]; //move to the end of center, i++ will then shift pointer to index right after current center
     }
-  
-    if (!palindrome) return null;
-    return palindrome;
-  }
+    return s.substring(start, end + 1);
+}
+
+function expandAroundCenter(s, left, right) {
+    let L = left, R = right;
+
+    while (L >= 0 && R < s.length && s[L] === s[R]) {
+        L--;
+        R++;
+    }
+    
+    return [++L, --R];
+}
+
+function getCenter(s,c){
+    let L = c, R = c;
+
+    while(s[L] === s[++R] && R <= s.length );
+
+    return [L, --R];
+}
+
+
+function isPalindrome(s) {
+    let k = 0;
+    let l = s.length - 1;
+    let isPalindrome = true;
+    while(k<=l) {
+        if (!(s.charAt(k) === s.charAt(l))) {
+            isPalindrome = false;
+            break;
+        }
+        k++;
+        l--;
+    }
+    return isPalindrome;
+}
+
+
+
+function longestPalindrome(s) {
+
+
+    let maxPalindrome = expandAroundCenterSolution(s);
+
+    return maxPalindrome;
+}
+
+let testCase = "akusukamakannasi";
+
+console.log(longestPalindrome(testCase))
